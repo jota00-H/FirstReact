4b66903b32acc2ad596da76a50bb5dd947c6f1d5
jota00-H <jh.a.camargo@hotmail.com>
2025-07-31 15:00:33 -0300

    Create BoxHome.js

---
+++ meuprojeto/src/Componentes/BoxHome.js
@@ -0,0 +1,12 @@
+import ParaBox from './ParaBox';
+
+function BoxHome(){
+    return(
+        <div>
+            <p>Você é pika meu irmão</p>
+            <ParaBox />
+        </div>
+    )
+}
+
+export default BoxHome
