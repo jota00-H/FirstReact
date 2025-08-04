f73bd633d3d8132723bc4f1d690276873569c20b
jota00-H <jh.a.camargo@hotmail.com>
2025-08-04 19:44:32 -0300

    Update App.js

--- meuprojeto/src/App.js
+++ meuprojeto/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import BoxHome from './Componentes/BoxHome';
-
+import ParaBox from './Componentes/ParaBox';
+  
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Parabéns, João Henrique!</h1>
         <BoxHome />
+        <ParaBox nome="joao" profissao = "medico" idade = "15" />
       </header>
     </div>
   );
